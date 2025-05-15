b09bdc02271b446afb28dbf17b6a694e98634daf
juxzpc <rasulshrfv@gmail.com>
2025-05-15 16:41:59 +0200

    Update my-publisher.sh

--- launchers/my-publisher.sh
+++ launchers/my-publisher.sh
@@ -6,7 +6,7 @@ source /environment.sh
 dt-launchfile-init
 
 # launch publisher
-rosrun my_package adaptive_speed_node.py
+rosrun my_package speed_node.py
 
 # wait for app to end
 dt-launchfile-join
